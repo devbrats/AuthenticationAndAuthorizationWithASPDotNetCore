5034addc283b0d5fab972a30b4e2f8fe45ac122f
Devbrat Singh <devvbrat1107@gmail.com>
2021-09-18 18:40:07 +0530

    OAuth with Jwt token demo project added

---
+++ AuthenticationAndAuthorizationWithAspDotNetCore/AAWithOAuth/Controllers/UserController.cs
@@ -0,0 +1,43 @@
+﻿using AAWithOAuth.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.IdentityModel.Tokens;
+using System;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+
+namespace AAWithOAuth.Controllers
+{
+    [Route("[controller]")]
+    [ApiController]
+    public class UserController : ControllerBase
+    {
+        [HttpGet("login")]
+        public IActionResult Login(string userName, string password)
+        {
+            // Create claims on login
+            var claims = new[]
+            {
+                new Claim(JwtRegisteredClaimNames.Sub, "test"),
+                new Claim("Test","TestID")
+            };
+
+            // Create Signing Credentials
+            var signingCredentials = new SigningCredentials(AuthorizationHelper.SymmetricKey, AuthorizationHelper.Algorithm);
+
+            // Creating Json Web Token
+            var token = new JwtSecurityToken(
+                AuthorizationHelper.Issuer,
+                AuthorizationHelper.Audience,
+                claims,
+                notBefore : DateTime.Now,
+                expires:DateTime.Now.AddHours(1),
+                signingCredentials
+                );
+
+            var tokenJson = new JwtSecurityTokenHandler().WriteToken(token);
+
+            return Ok( new { token = tokenJson } );
+        }
+
+    }
+}
---
+++ AuthenticationAndAuthorizationWithAspDotNetCore/AAWithOAuth/Startup.cs
@@ -0,0 +1,77 @@
+using AAWithOAuth.Authorization;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using Microsoft.IdentityModel.Tokens;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AAWithOAuth
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddAuthentication("OAuth")
+                .AddJwtBearer("OAuth", config =>
+                {
+                    // configuring to validate token as a part of query
+                    config.Events = new JwtBearerEvents()
+                    {
+                        OnMessageReceived = context =>
+                        {
+                            if (context.Request.Query.ContainsKey("access_token"))
+                            {
+                                context.Token = context.Request.Query["access_token"];
+                            }
+
+                            return Task.CompletedTask;
+                        }
+                    };
+
+                    config.TokenValidationParameters = new TokenValidationParameters()
+                    {
+                        ValidIssuer = AuthorizationHelper.Issuer,
+                        ValidAudience = AuthorizationHelper.Audience,
+                        IssuerSigningKey = AuthorizationHelper.SymmetricKey
+                    };
+                });
+            services.AddControllers();
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseHttpsRedirection();
+            app.UseRouting();
+            app.UseAuthentication();
+            app.UseAuthorization();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllers();
+            });
+        }
+    }
+}
